ece41e3f65c7516edb26c4ef56601680b07490df
Harry Salter <harry.salter@port.ac.uk>
2023-01-06 17:10:45 +0000

    Added sounds
    added generic player hits sounds and turret shooting sounds

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public GameObject projectile;
+    public float shootCooldown;
+    private float untilNextShot = 0;
+    private AudioSource turretAudioSource;
+    public AudioClip turretShoot;
+
+    void Start() 
+    {
+        turretAudioSource = GetComponent<AudioSource>();
+    }
+    
+    void FixedUpdate()
+    {
+        if (untilNextShot <= 0)
+        {
+            Shoot();
+            untilNextShot = shootCooldown;
+        }
+        untilNextShot -= Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        turretAudioSource.pitch = UnityEngine.Random.Range(0.9f, 1.1f);
+        turretAudioSource.PlayOneShot(turretShoot, 1.0f);
+        Instantiate(projectile, transform.position + transform.up * 0.75f + transform.forward * 0.5f, transform.rotation);
+    }
+}
