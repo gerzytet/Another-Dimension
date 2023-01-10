5992e97921f6d23901454360fd96ec73c34ddedd
laumi73 <laumichael73@gmail.com>
2023-01-09 23:23:44 -0500

    Revert "Added jetpack"
    This reverts commit 451655b182e3228f0f1b28b39c69ca8842a2b50d.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -27,8 +27,6 @@ public class Player : MonoBehaviour
     private int jetpackFuel;
     private RaycastHit groundedCheckRaycastHit;
     private int contacts = 0;
-    private bool isJetpackActive;
-    public float jetpackStrength;
 
     //Camera fields
     private Transform cameraPivot;
@@ -94,7 +92,6 @@ public class Player : MonoBehaviour
         this.CameraAngleConstant2D = Quaternion.AngleAxis(0, Vector3.up);
         dimensionTransitionFlag = false;
         this.jetpackFuel = jetpackMaxFuel;
-        this.isJetpackActive = false;
         
         instance = this;
         //Cursor.lockState = CursorLockMode.Locked;
@@ -162,10 +159,6 @@ public class Player : MonoBehaviour
             dimensionTransitionFlag = false;
         }
 
-        if (isJetpackActive){
-            print("Jetpack on");
-            rb.AddForce(Vector3.up * this.jetpackStrength, ForceMode.Impulse);
-        }
 
         checkDeath();
         jumpCooldown--;
@@ -201,21 +194,13 @@ public class Player : MonoBehaviour
         }
 
         //Jumping
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded() && jumpCooldown <= 0 && contacts > 0)
+        if (Input.GetKey(KeyCode.Space) && isGrounded() && jumpCooldown <= 0 && contacts > 0)
         {
             rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
             jumpCooldown = 20;
         }
 
         //Jetpack
-        if (Input.GetKey(KeyCode.Space) && !isGrounded() && jumpCooldown < 10)
-        {
-            this.isJetpackActive = true;
-        }
-
-        if (Input.GetKeyUp(KeyCode.Space)) {
-            this.isJetpackActive = false;
-        }
 
         //Switch Dimension Mode
         if (Input.GetKeyDown(KeyCode.Z))
